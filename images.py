d3a35a61765b01aba5aa34444f6a530a7b2029b9
Danail Koychev <danail.k.koychev@gmail.com>
2014-11-08 01:32:02 +0000

    add Game_of_luck and images

--- images.py
+++ images.py
@@ -2,4 +2,7 @@ import pygame
 
 images = {
             "top_wall" : pygame.image.load("images/top_wall.png"),
+            "bad_luck" : pygame.image.load("images/bad_luck.png"),
+            "good_luck" : pygame.image.load("images/good_luck.png"),
+            "arrow_of_fortune" : pygame.load("images/arrow_of_fortune")
         }
--- renderer.py
+++ renderer.py
@@ -3,5 +3,4 @@ from images import images
 
 def draw_everything(surface, objects):
     for obj in objects:
-        print(obj[0])
         surface.blit(images[obj[0]], (obj[1], obj[2]))
