2a4af7201102f846eaf284d2b0e2f325be8db974
Teodor Halvadzhiev <thalvadzhiev@gmail.com>
2014-11-08 08:48:07 +0200

    renderer

--- renderer.py
+++ renderer.py
@@ -4,4 +4,8 @@ from images import images
 def draw_everything(surface, objects):
     surface.fill((255, 255, 255))
     for obj in objects:
-        surface.blit(images[obj[0]], (obj[1], obj[2]))
+        if obj[0] == "clock":
+            clock = pygame.font.Font(None, 32)
+            surface.blit(clock.render(obj[1], 20, (0, 0, 0)), (obj[2], obj[3]))
+        else:
+            surface.blit(images[obj[0]], (obj[1], obj[2]))
