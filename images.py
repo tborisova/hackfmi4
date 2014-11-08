5cfed359d4e9bc181342663ab582f5306469e9ae
Danail Koychev <danail.k.koychev@gmail.com>
2014-11-08 01:46:38 +0000

    fix wrong funcion name

--- images.py
+++ images.py
@@ -4,5 +4,5 @@ images = {
             "top_wall" : pygame.image.load("images/top_wall.png"),
             "bad_luck" : pygame.image.load("images/bad_luck.png"),
             "good_luck" : pygame.image.load("images/good_luck.png"),
-            "arrow_of_fortune" : pygame.load("images/arrow_of_fortune")
+           # "arrow_of_fortune" : pygame.image.load("images/arrow_of_fortune.png")
         }
