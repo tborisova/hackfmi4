1290e0171a6ccc99b977a5433a012d692a4d73b2
Danail Koychev <danail.k.koychev@gmail.com>
2014-11-08 04:57:01 +0000

    add logo

--- images.py
+++ images.py
@@ -4,5 +4,6 @@ images = {
             "top_wall" : pygame.image.load("images/top_wall.png"),
             "bad_luck" : pygame.image.load("images/bad_luck.png"),
             "good_luck" : pygame.image.load("images/good_luck.png"),
+            "logo" : pygame.image.load("images/logo.png") 
            # "arrow_of_fortune" : pygame.image.load("images/arrow_of_fortune.png")
         }
