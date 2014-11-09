c5c0f879826e001011d5dd347b90a05544078b50
Stoyan <stoyaneft@gmail.com>
2014-11-09 14:59:48 +0200

    Fixed image bugs for kick_ball

--- kick_ball_client.py
+++ kick_ball_client.py
@@ -1,10 +1,7 @@
 from pygame.locals import *
 import pygame
-import math
-import json
 import sys
 
-from kick_ball_server import Game
 from settings import *
 from time import sleep
 from sys import stdin, exit
@@ -17,15 +14,15 @@ class Gui(ConnectionListener):
     def __init__(self, difficulty, host, port):
         pygame.init()
         pygame.display.set_caption('Kick Ball')
-        pygame.display.set_icon(pygame.image.load('./images/icon.png'))
+        pygame.display.set_icon(pygame.image.load(IMAGES_PATH + 'icon.png'))
         self.screen = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
         self.screen.fill((0, 0, 0))
         self.screen_center = (WINDOWWIDTH / 2, WINDOWHEIGHT / 2)
         self.clock = pygame.time.Clock()
-        self.font1 = pygame.font.Font('./data/font.ttf', 40)
-        self.font2 = pygame.font.Font('./data/font.ttf', 22)
-        self.font3 = pygame.font.Font('./data/font.ttf', 52)
-        self.font4 = pygame.font.Font('./data/font.ttf', 16)
+        self.font1 = pygame.font.Font(FONTS_PATH + 'font.tff', 40)
+        self.font2 = pygame.font.Font(FONTS_PATH + 'font.tff', 22)
+        self.font3 = pygame.font.Font(FONTS_PATH + 'font.tff', 52)
+        self.font4 = pygame.font.Font(FONTS_PATH + 'font.tff', 16)
         try:
             self.Connect((host, int(port)))
         except:
@@ -75,7 +72,7 @@ class Gui(ConnectionListener):
                 connection.Send({'action': 'mouse_pos', 'x': x, 'y': y})
 
     def draw_message(self, message):
-        font = pygame.font.Font('./data/font.ttf', 52)
+        font = pygame.font.Font('./data/fonts/font.ttf', 52)
         label = font.render(message, 1, WHITE)
         rect = label.get_rect()
         rect.center = CENTER
@@ -97,7 +94,7 @@ class Gui(ConnectionListener):
         self.screen.blit(highscore_text, highscore_rect)
         self.screen.blit(title, title_rect)
         rect = Rect(data['ball_rect_x'], data['ball_rect_y'], data['ball_rect_w'], data['ball_rect_h'])
-        img = pygame.image.load('./images/ball.png')
+        img = pygame.image.load(IMAGES_PATH + 'ball.png')
         rotated = pygame.transform.rotate(img, data['newimg_angle'])
         size = rotated.get_size()
         subrect = img.get_rect()
@@ -108,13 +105,10 @@ class Gui(ConnectionListener):
         rotated_img = rotated.subsurface(subrect)
         self.screen.blit(rotated_img, rect) #self.game.ball.rect)
 
-if __name__ == "__main__": 
-    if len(sys.argv) != 2:
-        print("Usage: {0} host:port".format(sys.argv[0]))
-        print("e.g. {0} localhost:31425",sys.argv[0])
-    else:
-        host, port = sys.argv[1].split(":")
-        c = Gui(10, host, int(port))
-        while 1:
-            c.Loop()
-            sleep(0.001)
+if __name__ == "__main__":
+    host = '10.0.201.111'
+    port = 12022
+    c = Gui(10, host, int(port))
+    while 1:
+        c.Loop()
+        sleep(0.001)
